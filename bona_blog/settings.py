9cc1a54e4b0cdf710d1e3ad987c26a4e9b01b75c
abioduog <abioduog@gmail.com>
2024-09-29 22:13:15 +0100

    Update project files and settings

--- bona_blog/settings.py
+++ bona_blog/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.vercel.app']
 
 # Add Vercel URL to allowed hosts
 ALLOWED_HOSTS += ['.vercel.app']
---
+++ build_files.sh
@@ -0,0 +1,4 @@
+   # build_files.sh
+   #!/bin/bash
+   pip install -r requirements.txt
+   python manage.py collectstatic --noinput
